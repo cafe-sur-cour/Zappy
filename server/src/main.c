5574fe79f241dfc2af103098be1472e3b12383eb
Matisse-M <matisse.marsac@epitech.eu>
2025-05-27 10:04:10 +0200

    ✨: Server: Add basis of server side including Makefile and basic main

---
+++ server/src/main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** Server
+** File description:
+** Server :: Main
+*/
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("\033[1;34m[SERVER]\033[0m ");
+    printf("Welcome to the Tranttorian world!\n");
+    return 0;
+}
