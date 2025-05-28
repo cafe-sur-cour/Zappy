0ca061ed4cfb3585e5acae8827fd8d32a8ffe208
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-28 19:44:51 +0200

    :sparkles: GUI: Client class empty implementation

---
+++ gui/src/Client/Client.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** Client
+*/
+
+#include <iostream>
+#include "Client.hpp"
+
+Client::Client(int ac, char **av)
+{
+    (void)ac;
+    (void)av;
+}
+
+Client::~Client()
+{
+}
