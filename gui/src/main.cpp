93e69acd6b6fb37f55872a49837fa6aa858e26fe
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-28 19:45:43 +0200

    :sparkles: GUI: Main handling -help and try catch

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -6,12 +6,27 @@
 */
 
 #include <iostream>
+
 #include "Utils/Constants.hpp"
+#include "Client/Client.hpp"
+#include "Exceptions/Exceptions.hpp"
 
 int main(int ac, char **av)
 {
-    (void)ac;
-    (void)av;
-    std::cout << zappy::constants::USAGE_STRING << std::endl;
-    return 0;
+    if (ac == 2 && std::string(av[1]) == "-help") {
+        std::cout << zappy::constants::USAGE_STRING << std::endl;
+        return zappy::constants::SUCCESS_EXIT_CODE;
+    }
+
+    try {
+        Client client(ac, av);
+    } catch (const Exceptions::CLIParsingException &e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << zappy::constants::USAGE_STRING << std::endl;
+        return zappy::constants::FAILURE_EXIT_CODE;
+    } catch (const std::exception &e) {
+        std::cerr << "An unexpected error occurred: " << e.what() << std::endl;
+        return zappy::constants::FAILURE_EXIT_CODE;
+    }
+    return zappy::constants::SUCCESS_EXIT_CODE;
 }
