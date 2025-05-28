d621b938f0e2faf4e6e1d0d07d62b7366a6455a5
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-28 19:29:13 +0200

    :sparkles: GUI/Utils: USAGE_STRING constant

---
+++ gui/src/Utils/Constants.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** Constants
+*/
+
+#ifndef CONSTANTS_HPP_
+#define CONSTANTS_HPP_
+
+namespace zappy::constants {
+
+    const char *USAGE_STRING = "USAGE: ./zappy_gui -p port -h machine\n"
+                               "option\t\tdescription\n"
+                               "port\t\tport number\n"
+                               "machine\t\thostname of the server";
+}
+
+#endif /* !CONSTANTS_HPP_ */
