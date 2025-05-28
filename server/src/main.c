a2048144a9c2e63f9e720eb638a98735a202066d
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-28 19:21:29 +0200

    :sparkles: Server: empty main

---
+++ server/src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** main
+*/
+
+int main(int ac, char **av)
+{
+    (void)ac;
+    (void)av;
+    return 0;
+}
