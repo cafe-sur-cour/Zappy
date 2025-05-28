45673ede0d4c603fa08223a15e9f8603544cd802
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-28 19:21:43 +0200

    :sparkles: GUI: empty main

---
+++ gui/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    (void)ac;
+    (void)av;
+    std::cout << "Zappy GUI is running!" << std::endl;
+    return 0;
+}
