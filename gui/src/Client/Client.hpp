781a4d3ed90810b78996640b31b6d5640c66d96e
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-28 19:44:41 +0200

    :sparkles: GUI: Client class empty definition

---
+++ gui/src/Client/Client.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** Client
+*/
+
+#ifndef CLIENT_HPP_
+#define CLIENT_HPP_
+
+class Client {
+    public:
+        Client(int ac, char **av);
+        ~Client();
+};
+
+#endif /* !CLIENT_HPP_ */
