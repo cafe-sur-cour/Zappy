57d12f51cea93a64a20eaff62ffdf81cdf51a6b0
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-30 18:17:43 +0200

    :sparkles: AI: print usage

---
+++ ai/src/Utils/Utils.py
@@ -0,0 +1,12 @@
+##
+## EPITECH PROJECT, 2025
+## B-YEP-400-NAN-4-1-zappy-albane.merian
+## File description:
+## Utils
+##
+
+USAGE_STRING = "Usage: ./zappy_ai -p port -n name -h machine\n" \
+               "option\t\tdescription\n" \
+               "-p port\t\tport number\n" \
+               "-n name\t\tname of the team\n" \
+               "-h machine\tname of the machine; localhost by default"
---
+++ ai/src/main.py
@@ -0,0 +1,11 @@
+##
+## EPITECH PROJECT, 2025
+## B-YEP-400-NAN-4-1-zappy-albane.merian
+## File description:
+## main
+##
+
+from src.Utils.Utils import USAGE_STRING
+
+def main():
+    print(USAGE_STRING)
