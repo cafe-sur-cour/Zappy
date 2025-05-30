926fc1fa27d135e3c0836613bd785b6268418dd6
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-30 15:01:23 +0200

    :sparkles: GUI: GameInfos.cpp -> map size

---
+++ gui/src/Game/GameInfos.cpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** GameInfos
+*/
+
+#include "GameInfos.hpp"
+
+GameInfos::GameInfos() :
+    _mapWidth(0),
+    _mapHeight(0)
+{
+}
+
+GameInfos::~GameInfos()
+{
+}
+
+void GameInfos::setMapSize(int width, int height)
+{
+    _mapWidth = width;
+    _mapHeight = height;
+}
+
+std::pair<int, int> GameInfos::getMapSize() const
+{
+    return std::make_pair(_mapWidth, _mapHeight);
+}
