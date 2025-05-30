39a90fb17f0545adeb7948e434c216271303cb1d
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-30 18:55:43 +0200

    :sparkles: colors to utils

--- ai/src/Utils/Utils.py
+++ ai/src/Utils/Utils.py
@@ -10,3 +10,15 @@ USAGE_STRING = "Usage: ./zappy_ai -p port -n name -h machine\n" \
                "-p port\t\tport number\n" \
                "-n name\t\tname of the team\n" \
                "-h machine\tname of the machine; localhost by default"
+
+
+class Colors:
+    BOLD = "\033[1m"
+    RED = "\033[1m\033[31m"
+    GREEN = "\033[1m\033[32m"
+    YELLOW = "\033[1m\033[33m"
+    BLUE = "\033[1m\033[34m"
+    MAGENTA = "\033[1m\033[35m"
+    CYAN = "\033[1m\033[36m"
+    WHITE = "\033[1m\033[37m"
+    RESET = "\033[0m"
