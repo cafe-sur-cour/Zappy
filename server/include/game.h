d69ad94eb18c6001fecad8bbcba4df9d843403b0
Albane Merian <albane.merian@epitech.eu>
2025-06-02 17:13:58 +0200

    ✨ : Server : Add the declaration of the different data structure we will use

---
+++ server/include/game.h
@@ -0,0 +1,88 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** game
+*/
+
+#include <time.h>
+
+#ifndef GAME_H_
+    #define GAME_H_
+
+/* Definition of the directions */
+enum direction_e {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST
+};
+
+/* definintion od the different element on the map */
+enum crystal_e {
+    LINEMATE,
+    DERAUMERE,
+    SIBUR,
+    MENDIANE,
+    PHIRAS,
+    THYSTAME
+};
+
+/* Struct that "counts" the current stat of the player */
+typedef struct lives_s {
+    int freq;
+    int nbFood;
+    time_t startRefresh;
+    time_t endRefresh;
+} lives_t;
+
+/* Struct defining the invetory of the player */
+typedef struct inventory_s {
+    int nbLinemate;
+    int nbDeraumere;
+    int nbSibur;
+    int nbMendiane;
+    int nbPhiras;
+    int nbThystame;
+} inventory_t;
+
+/* Player struct */
+typedef struct player_s {
+    int id; /* This is equal to the current FD */
+    int level;
+    int posX;
+    int posY;
+    bool isAlive;
+    direction_e direction;
+    inventory_t *inventory;
+    lives_t *lives;
+    player_t *next;
+} player_t;
+
+/* Team Strcut */
+typedef struct team_s {
+    char *name;
+    int nbPlayers;
+    int nbPlayerAlive;
+    player_t *players;
+    team_t *next;
+} team_t;
+
+/* Ressources, and there pos on the map */
+typedef struct ressources_s {
+    crystal_e type;
+    int posX;
+    int posY;
+    ressources_t *next;
+} ressources_t;
+
+/* Map struct */
+typedef struct map_s {
+    int width;
+    int heigt;
+    team_t *teams;
+    ressources_t *ressources;
+} map_t;
+
+#endif /* !GAME_H_ */
+
