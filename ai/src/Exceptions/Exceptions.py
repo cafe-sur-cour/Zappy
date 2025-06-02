4f0df3743666820f3cb9faffbde30773e71ecee9
albanrss <alban.roussee@epitech.eu>
2025-06-02 15:21:03 +0200

    :recycle: AI: update headers for cleaner look, and removed useless imports

--- ai/src/Exceptions/Exceptions.py
+++ ai/src/Exceptions/Exceptions.py
@@ -1,6 +1,6 @@
 ##
 ## EPITECH PROJECT, 2025
-## B-YEP-400-NAN-4-1-zappy-albane.merian
+## zappy
 ## File description:
 ## Exceptions
 ##
--- ai/src/main.py
+++ ai/src/main.py
@@ -1,6 +1,6 @@
 ##
 ## EPITECH PROJECT, 2025
-## B-YEP-400-NAN-4-1-zappy-albane.merian
+## zappy
 ## File description:
 ## main
 ##
@@ -8,7 +8,7 @@
 from src.Utils.Utils import USAGE_STRING, Colors
 from src.CLI.CLI import CLI
 from src.Exceptions.Exceptions import CLIParsingException
-from sys import argv, exit
+from sys import argv
 
 
 def main():
