b77da7c4f2bc1fe67264cc50b7a603e744589253
Nolan Papa <nolan.papa@epitech.eu>
2025-06-03 19:11:37 +0200

    🐛 Fix Containers constructor and member variable to use shared pointer for RayLib

--- gui/src/Graphic/HUD/Containers/Containers.hpp
+++ gui/src/Graphic/HUD/Containers/Containers.hpp
@@ -37,7 +37,7 @@ class Containers : public AContainers {
          * @param height Container height
          * @param backgroundColor Background color (default: semi-transparent dark gray)
          */
-        Containers(RayLib& raylib, float x, float y, float width, float height, Color backgroundColor = {40, 40, 40, 200});
+        Containers(std::shared_ptr<RayLib> raylib, float x, float y, float width, float height, Color backgroundColor = {40, 40, 40, 200});
 
         /**
          * @brief Destroy the Container
@@ -191,7 +191,7 @@ class Containers : public AContainers {
         void handleResize(int oldWidth, int oldHeight, int newWidth, int newHeight);
 
     private:
-        RayLib& _raylib;
+        std::shared_ptr<RayLib> _raylib;
         Texture2D _backgroundTexture;
         bool _hasBackgroundTexture;
         std::unordered_map<std::string, std::shared_ptr<IUIElement>> _elements;
