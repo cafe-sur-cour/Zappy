f7ebe1bffd4e35b70066c7443fa3a13db7a37837
Albane Merian <albane.merian@epitech.eu>
2025-06-03 11:09:39 +0200

    ♻️  : Server : Update the server free so that it is in an indepandent file

---
+++ server/src/free.c
@@ -0,0 +1,104 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** free
+*/
+#include "algo.h"
+#include "zappy.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+
+void *free_params(params_t *params)
+{
+    if (!params)
+        return NULL;
+    if (params->teams) {
+        for (int i = 0; i < params->nb_team; i++) {
+            free(params->teams[i]);
+        }
+        free(params->teams);
+    }
+    free(params);
+    return NULL;
+}
+
+static void free_ressources(ressources_t *ressources)
+{
+    ressources_t *current = ressources;
+    ressources_t *next;
+
+    while (current) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+}
+
+static void free_players(player_t *player)
+{
+    player_t *next_player;
+    while (player) {
+        next_player = player->next;
+        if (player->inventory) {
+            free(player->inventory);
+        }
+        if (player->lives) {
+            free(player->lives);
+        }
+        free(player);
+        player = next_player;
+    }
+}
+
+static void free_teams(team_t *teams)
+{
+    team_t *current = teams;
+    team_t *next;
+
+    while (current) {
+        next = current->next;
+        if (current->name) {
+            free(current->name);
+        }
+        free_players(current->players);
+        free(current);
+        current = next;
+    }
+}
+
+static void free_map(map_t *map)
+{
+    if (!map)
+        return;
+    free_teams(map->teams);
+    free_ressources(map->ressources);
+    free(map);
+}
+
+static void free_graph(graph_t *graph)
+{
+    if (!graph)
+        return;
+    if (graph->pollfd) {
+        free(graph->pollfd);
+    }
+    free(graph);
+}
+
+void *free_server(server_t *server)
+{
+    if (!server)
+        return NULL;
+    if (server->params)
+        free_params(server->params);
+    if (server->sockfd != -1)
+        close(server->sockfd);
+    if (server->map)
+        free_map(server->map);
+    if (server->graph)
+        free_graph(server->graph);
+    free(server);
+    return NULL;
+}
