50276be3d16461873c62c69e3f15a37ebdf7e4ce
Nolan Papa <nolan.papa@epitech.eu>
2025-06-03 18:49:15 +0200

    ✨ Add Text class for rendering and managing text elements in the UI

---
+++ gui/src/Graphic/HUD/Text/Text.cpp
@@ -0,0 +1,72 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** Text
+*/
+
+#include "Text.hpp"
+
+Text::Text(
+    RayLib& raylib,
+    float x, float y,
+    const std::string& text,
+    float fontSize,
+    Color color
+) : AUIElement(x, y, raylib.measureText(text, fontSize), fontSize),
+    _text(text),
+    _fontSize(fontSize),
+    _color(color),
+    _raylib(raylib)
+{
+    _bounds.width = _raylib.measureText(_text, _fontSize);
+    _bounds.height = fontSize;
+}
+
+void Text::draw()
+{
+    if (!_visible)
+        return;
+
+    _raylib.drawText(_text, _bounds.x, _bounds.y, _fontSize, _color);
+}
+
+void Text::update()
+{
+
+}
+
+void Text::setText(const std::string& text)
+{
+    _text = text;
+
+    _bounds.width = _raylib.measureText(_text, _fontSize);
+}
+
+std::string Text::getText() const
+{
+    return _text;
+}
+
+void Text::setFontSize(float fontSize)
+{
+    _fontSize = fontSize;
+
+    _bounds.width = _raylib.measureText(_text, _fontSize);
+    _bounds.height = _fontSize;
+}
+
+float Text::getFontSize() const
+{
+    return _fontSize;
+}
+
+void Text::setColor(Color color)
+{
+    _color = color;
+}
+
+Color Text::getColor() const
+{
+    return _color;
+}
