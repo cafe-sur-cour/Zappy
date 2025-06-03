3f3f23533aba638d486467a25a0f74bb15475c70
Albane Merian <albane.merian@epitech.eu>
2025-06-03 16:13:47 +0200

    ✨ : Server : Add the sending of the map_size_to the gui

--- server/src/gui_send_message.c
+++ server/src/gui_send_message.c
@@ -5,3 +5,29 @@
 ** gui_send_message
 */
 
+#include "zappy.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+// void send_entrie_map(server_t *server)
+// {
+
+// }
+
+// void send_map_tile(server_t *sever)
+// {
+
+// }
+
+
+void send_map_size(server_t *server)
+{
+    int xLenthth = int_str_len(server->map->width) +
+        int_str_len(server->map->heigt) + 7;
+    char *message = malloc(sizeof(char) * xLenthth);
+    char *x = my_itoa(server->map->width);
+    char *y = my_itoa(server->map->heigt);
+
+    snprintf(message, xLenthth, "msz %s %s\n", x, y);
+    write_message(server->graph->fd, message);
+}
