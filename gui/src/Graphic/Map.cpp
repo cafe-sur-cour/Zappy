61924ef4a82367c5a43cf988766eed603f7c26a9
eliotttesnier <eliott.tesnier@epitech.eu>
2025-06-03 16:29:43 +0200

    :sparkles: GUI: Basic display of players

--- gui/src/Graphic/Map.cpp
+++ gui/src/Graphic/Map.cpp
@@ -7,6 +7,8 @@
 
 #include <memory>
 #include <utility>
+#include <unordered_map>
+#include <algorithm>
 #include "RayLib/RayLib.hpp"
 #include "Map.hpp"
 
@@ -19,12 +21,26 @@ Map::~Map()
 {
 }
 
+Color Map::getTeamColor(const std::string &teamName)
+{
+    if (_teamColors.find(teamName) == _teamColors.end()) {
+        _teamColors[teamName] = {
+            static_cast<unsigned char>(rand() % 200 + 55),
+            static_cast<unsigned char>(rand() % 200 + 55),
+            static_cast<unsigned char>(rand() % 200 + 55),
+            255
+        };
+    }
+    return _teamColors[teamName];
+}
+
 void Map::draw()
 {
     auto tiles = _gameInfos->getTiles();
 
     for (const auto &tile : tiles) {
         drawTile(tile.x, tile.y, tile);
+        drawPlayers(tile.x, tile.y);
     }
 }
 
@@ -35,4 +51,70 @@ void Map::drawTile(int x, int y, const zappy::structs::Tile &tile)
     (void)tile;
     _raylib->drawCube(position, 0.9f, 0.2f, 0.9f, LIGHTGRAY);
     _raylib->drawCubeWires(position, 0.9f, 0.2f, 0.9f, BLACK);
+}
+
+void Map::drawPlayers(int x, int y)
+{
+    const auto& players = _gameInfos->getPlayers();
+    std::vector<const zappy::structs::Player*> playersOnTile;
+
+    for (const auto& player : players) {
+        if (player.x == x && player.y == y) {
+            playersOnTile.push_back(&player);
+        }
+    }
+
+    float cylinderHeight = 0.4f;
+    float cylinderRadius = 0.25f;
+    float baseHeight = 0.1f;
+
+    for (size_t i = 0; i < playersOnTile.size(); ++i) {
+        Vector3 position = {
+            static_cast<float>(x),
+            baseHeight + cylinderHeight * (i + 0.5f),
+            static_cast<float>(y)
+        };
+
+        Color teamColor = getTeamColor(playersOnTile[i]->teamName);
+        _raylib->drawCylinder(position, cylinderRadius, cylinderRadius,
+                             cylinderHeight, 12, teamColor);
+        _raylib->drawCylinderWires(position, cylinderRadius, cylinderRadius,
+                                  cylinderHeight, 12, BLACK);
+        drawOrientationArrow(position, playersOnTile[i]->orientation,
+            cylinderHeight);
+    }
+}
+
+void Map::drawOrientationArrow(const Vector3 &position, int orientation, float playerHeight)
+{
+    float arrowLength = 0.35f;
+    float arrowWidth = 0.15f;
+    float arrowHeadSize = 0.2f;
+
+    Vector3 arrowPos = position;
+    arrowPos.y += playerHeight / 2 + 0.15f;
+    Vector3 start = arrowPos;
+    Vector3 end = arrowPos;
+
+    int direction = orientation - 1;
+    if (direction < 0) direction = 0;
+
+    switch (direction) {
+        case 0: end.z -= arrowLength; break;
+        case 1: end.x += arrowLength; break;
+        case 2: end.z += arrowLength; break;
+        case 3: end.x -= arrowLength; break;
+    }
+    _raylib->drawCylinderEx(start, end, 0.05f, 0.05f, 8, RED);
+
+    Vector3 coneStart = end;
+    Vector3 coneEnd;
+
+    switch (direction) {
+        case 0: coneEnd = {end.x, end.y, end.z - arrowHeadSize}; break;
+        case 1: coneEnd = {end.x + arrowHeadSize, end.y, end.z}; break;
+        case 2: coneEnd = {end.x, end.y, end.z + arrowHeadSize}; break;
+        case 3: coneEnd = {end.x - arrowHeadSize, end.y, end.z}; break;
+    }
+    _raylib->drawCylinderEx(coneStart, coneEnd, arrowWidth, 0.0f, 8, RED);
 }
--- gui/src/Graphic/Map.hpp
+++ gui/src/Graphic/Map.hpp
@@ -9,6 +9,8 @@
 #define MAP_HPP_
 
 #include <memory>
+#include <unordered_map>
+#include <string>
 #include "../Game/GameInfos.hpp"
 #include "RayLib/RayLib.hpp"
 
@@ -19,10 +21,15 @@ class Map {
 
         void draw();
         void drawTile(int x, int y, const zappy::structs::Tile &tile);
+        void drawPlayers(int x, int y);
+        Color getTeamColor(const std::string &teamName);
 
     private:
         std::shared_ptr<GameInfos> _gameInfos;
         std::shared_ptr<RayLib> _raylib;
+        std::unordered_map<std::string, Color> _teamColors;
+
+        void drawOrientationArrow(const Vector3 &position, int orientation, float playerHeight);
 };
 
 #endif /* !MAP_HPP_ */
