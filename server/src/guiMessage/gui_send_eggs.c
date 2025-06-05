0b929ca3d76d240afffb9516324060300d2b170e
Albane Merian <albane.merian@epitech.eu>
2025-06-05 16:38:29 +0200

    ✨ : Server : Add all the messages that send information about eggs to the gui client

--- server/src/guiMessage/gui_send_eggs.c
+++ server/src/guiMessage/gui_send_eggs.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* This send the information of one egg */
 void send_egg(zappy_t *zappy, egg_t *egg)
 {
     egg_t *current = egg;
@@ -30,6 +31,7 @@ void send_egg(zappy_t *zappy, egg_t *egg)
     free(message);
 }
 
+/* This function sends the list of the entire list of the egg of the map */
 void send_entire_egg_list(zappy_t *zappy)
 {
     egg_t *current = zappy->game->map->currentEggs;
@@ -38,4 +40,22 @@ void send_entire_egg_list(zappy_t *zappy)
         send_egg(zappy, current);
         current = current->next;
     }
+}
+
+/* This function sends the message that a playr is laying an egg */
+void send_player_laying_egg(zappy_t *zappy, player_t *player)
+{
+    int xLenght = int_str_len(player->id) + 7;
+    char *message = malloc(sizeof(char) * xLenght);
+
+    if (message == NULL) {
+        error_message("Failed to allocate memory for player laying egg.");
+        return;
+    }
+    snprintf(message, xLenght, "pfk #%d\n", player->id);
+    if (zappy->params->is_debug == true) {
+        printf("Sending to GUI: %s", message);
+    }
+    write_message(zappy->graph->fd, message);
+    free(message);
 }
