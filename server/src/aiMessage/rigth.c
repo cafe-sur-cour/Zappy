89e1016dfb93b6f3ce18fcaaaa5bd6f3bf9002d5
Matisse Marsac <matisse.marsac@epitech.eu>
2025-06-08 10:20:15 +0200

    ✨ : Server : Add the working function of the right commands

--- server/src/aiMessage/rigth.c
+++ server/src/aiMessage/rigth.c
@@ -12,9 +12,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int right_message(player_t *player)
+{
+    direction_t direction = player->direction;
+
+    if (direction == NORTH)
+        player->direction = WEST;
+    if (direction == WEST)
+        player->direction = SOUTH;
+    if (direction == SOUTH)
+        player->direction = EAST;
+    if (direction == EAST)
+        player->direction = NORTH;
+    else
+        return -1;
+    return 0;
+}
+
 int handle_right(player_t *player, char *command, zappy_t *zappy)
 {
     (void)command;
+    if (right_message(player) == -1)
+        return -1;
     if (send_player_pos(zappy, player) == -1)
         return -1;
     return 0;
