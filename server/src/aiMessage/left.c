a0627be843bdbae2a633cca89ead7337a75ba03e
Matisse Marsac <matisse.marsac@epitech.eu>
2025-06-08 10:19:07 +0200

    ✨ : Server : Add the workign of the left function so that the server uses it correctly

--- server/src/aiMessage/left.c
+++ server/src/aiMessage/left.c
@@ -12,10 +12,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+int left_message(player_t *player)
+{
+    direction_t direction = player->direction;
+
+    if (direction == NORTH)
+        player->direction = EAST;
+    if (direction == EAST)
+        player->direction = SOUTH;
+    if (direction == SOUTH)
+        player->direction = WEST;
+    if (direction == WEST)
+        player->direction = NORTH;
+    else
+        return -1;
+    return 0;
+}
+
 /* This function is defined in the array of funcion pointers */
 int handle_left(player_t *player, char *command, zappy_t *zappy)
 {
     (void)command;
+    if (left_message(player) == -1)
+        return -1;
     if (send_player_pos(zappy, player) == -1)
         return -1;
     return 0;
