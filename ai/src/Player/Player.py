955d5f7c0ad4787a597fb891b434662cc2898a0d
albanrss <alban.roussee@epitech.eu>
2025-06-09 16:35:41 +0200

    :recycle: Player : Refactor player actions and state management, enhance roomba behavior and response handling

--- ai/src/Player/Player.py
+++ ai/src/Player/Player.py
@@ -6,51 +6,75 @@
 #
 
 import os
-from random import randint
 from threading import Thread
 from time import sleep
+
 from src.Hash.Hash import Hash
 from src.Exceptions.Exceptions import (
     CommunicationException
 )
-
 from src.Communication.Communication import Communication
 from src.Utils.Utils import SUCCESS, FAILURE
+
+MIN_FOOD = 10
+
+LVL_UPGRADES = {
+    1: {"linemate": 1, "deraumere": 1, "sibur": 0, "mendiane": 0, "phiras": 0, "thystame": 0},
+    2: {"linemate": 1, "deraumere": 1, "sibur": 1, "mendiane": 0, "phiras": 0, "thystame": 0},
+    3: {"linemate": 2, "deraumere": 0, "sibur": 1, "mendiane": 0, "phiras": 2, "thystame": 0},
+    4: {"linemate": 1, "deraumere": 1, "sibur": 2, "mendiane": 0, "phiras": 1, "thystame": 0},
+    5: {"linemate": 1, "deraumere": 2, "sibur": 1, "mendiane": 3, "phiras": 0, "thystame": 0},
+    6: {"linemate": 1, "deraumere": 2, "sibur": 3, "mendiane": 0, "phiras": 1, "thystame": 0},
+    7: {"linemate": 2, "deraumere": 2, "sibur": 2, "mendiane": 2, "phiras": 2, "thystame": 1},
+}
 
 
 class Player:
     def __init__(self, name: str, ip: str, port: int = 4242) -> None:
         self.communication: Communication = Communication(name, ip, port)
+        self._commThread: Thread = Thread(
+            target=self.communication.loop,
+            name=f"CommunicationThread-{name}"
+        )
+
         self.childs: list[int] = []
+
         self.teamName: str = name
         self.ip: str = ip
         self.port: int = port
+
         self.level: int = 1
         self.hash: Hash = Hash(name)
         self.inventory: dict[str, int] = {
-            "food": 10, "linemate": 0, "deraumere": 0, "sibur": 0,
-            "mendiane": 0, "phiras": 0, "thystame": 0
+            "food": 10,
+            "linemate": 0,
+            "deraumere": 0,
+            "sibur": 0,
+            "mendiane": 0,
+            "phiras": 0,
+            "thystame": 0
         }
-        self.in_incantation: bool = False
-        self.x = 0
-        self.y = 0
-        self._commThread: Thread = Thread(
-            target=self.communication.loop,
-            name=f"CommunicationThread-{self.teamName}"
-        )
+        self.look: list[dict[str, int]] = []
+        self.inIncantation: bool = False
+        self.x: int = 0
+        self.y: int = 0
 
-    def __del__(self):
-        if len(self.childs) == 0:
-            return
-        for pid in self.childs:
-            os.waitpid(pid, os.WNOHANG)
+        self.roombaState: dict = {
+                "forwardCount": 0,
+                "targetForward": 10,
+                "phase": "forward",
+                "waitingForResponse": False,
+                "lastCommand": None
+            }
 
     def __str__(self):
-        return (f"Player team: {self.teamName}, "
-                f"Level: {self.level}, "
-                f"Inventory: {self.inventory}, "
-                f"Alive: {not self.communication.is_dead()}, "
-                f"In Incantation: {self.in_incantation}")
+        return (
+            f"Player team: {self.teamName}, "
+            f"Level: {self.level}, "
+            f"Inventory: {self.inventory}, "
+            f"Alive: {not self.communication.playerIsDead()}, "
+            f"In Incantation: {self.inIncantation}"
+        )
 
     def create_child(self) -> int:
         pid: int = os.fork()
@@ -72,23 +96,95 @@ class Player:
     def setMapSize(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
+        self.roombaState["targetForward"] = max(x, y)
+
+    def getNeededStonesByPriority(self) -> list[str]:
+        neededStones = []
+        for stone, quantity in LVL_UPGRADES[self.level].items():
+            neededQuantity = quantity - self.inventory.get(stone, 0)
+            neededStones.append((neededQuantity, stone))
+        neededStones.sort(key=lambda x: x[0], reverse=True)
+        neededStones = [stone for quantity, stone in neededStones if quantity > 0]
+        return neededStones
+
+    def roombaAction(self) -> None:
+        if self.roombaState["waitingForResponse"]:
+            return
+        
+        if self.roombaState["phase"] == "forward":
+            if (
+                self.roombaState["lastCommand"] == "left"
+                or self.roombaState["lastCommand"] == "forward"
+                or self.roombaState["lastCommand"] == None
+            ):
+                self.communication.sendLook()
+                self.roombaState["lastCommand"] = "look"
+                self.roombaState["waitingForResponse"] = True
+
+            elif self.roombaState["lastCommand"] == "look":
+                self.look = self.communication.getLook() or self.look
+                if "food" in self.look[0].keys():
+                    self.communication.sendTakeObject("food")
+                neededStones = self.getNeededStonesByPriority()
+                for stone in neededStones:
+                    if stone in self.look[0].keys():
+                        self.communication.sendTakeObject(stone)
+                self.roombaState["lastCommand"] = "getObjects"
+                self.roombaState["waitingForResponse"] = True
+
+            elif self.roombaState["lastCommand"] == "getObjects":
+                if self.roombaState["forwardCount"] < self.roombaState["targetForward"]:
+                    self.communication.sendForward()
+                    self.roombaState["lastCommand"] = "forward"
+                    self.roombaState["waitingForResponse"] = True
+                    self.roombaState["forwardCount"] += 1
+
+                else:
+                    self.roombaState["forwardCount"] = 0
+                    self.roombaState["phase"] = "turn"
+                    self.communication.sendRight()
+                    self.roombaState["lastCommand"] = "right"
+                    self.roombaState["waitingForResponse"] = True
+
+        elif self.roombaState["phase"] == "turn":
+            if self.roombaState["lastCommand"] == "right":
+                self.communication.sendForward()
+                self.roombaState["lastCommand"] = "forward"
+                self.roombaState["waitingForResponse"] = True
+
+            elif self.roombaState["lastCommand"] == "forward":
+                self.communication.sendLeft()
+                self.roombaState["lastCommand"] = "left"
+                self.roombaState["phase"] = "forward"
+                self.roombaState["waitingForResponse"] = True
+
+    def handleCommandResponse(self, response: str) -> None:
+        if response.strip() == "inventory":
+            self.inventory = self.communication.getInventory()
+
+        if response.strip() == "look":
+            self.look = self.communication.getLook()
+
+        elif response.strip() == "ko":
+            print(f"Command '{self.roombaState["lastCommand"]}' failed")
+
+        if not self.communication.hasPendingCommands():
+            self.roombaState["waitingForResponse"] = False
 
     def loop(self) -> None:
-        while not self.communication.is_dead():
-            direction = randint(0, 3)
-            if direction == 0:
-                self.communication.sendLeft()
-            elif direction == 1:
-                self.communication.sendRight()
-            self.communication.sendForward()
+        while not self.communication.playerIsDead():
+            if self.communication.hasMessages():
+                message = self.communication.getLastMessage()
+                response = self.hash.unHashMessage(str(message[1][:-1]))
+                print(f"Message received from the server: {response}")
 
-            newInventory = self.communication.getInventory()
-            if newInventory:
-                self.inventory = newInventory
+            if self.communication.hasReponses():
+                response = self.communication.getLastResponse()
+                if response.strip() == "dead":
+                    break
+                self.handleCommandResponse(response)
 
-            if self.communication.get_size_message_queue() > 0:
-                message = self.communication.get_message_from_queue()
-                print(
-                    "Message received from the server "
-                    f"{self.hash.unHashMessage(str(message[1][:-1]))}"
-                )
+            if not self.communication.hasPendingCommands():
+                self.roombaAction()
+
+            sleep(0.1)
