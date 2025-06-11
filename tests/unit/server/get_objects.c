1a03ce3e9cf1f3ecc8f29c2e9859f5ac13fedc20
Albane Merian <albane.merian@epitech.eu>
2025-06-11 12:12:54 +0200

    ✅ : Server : Add unit test to test the set Object command handler

---
+++ tests/unit/server/get_objects.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** get_objects
+*/
+
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "zappy.h"
+#include "network.h"
+
+
+static void redirect_all_std(void)
+{
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
