d06436efa4712b3fcb21a835fe1619527846f436
Nolan Papa <nolan.papa@epitech.eu>
2025-06-12 16:59:36 +0200

    ✨ Add SimpleSkybox drawing method to Raylib class

---
+++ gui/src/RayLib/SimpleSkyboxMethods.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-NAN-4-1-zappy-albane.merian
+** File description:
+** RayLibSimpleSkybox
+*/
+
+#include "Raylib.hpp"
+
+void Raylib::drawSimpleSkybox()
+{
+    this->_raylib->drawSimpleSkybox();
+}
