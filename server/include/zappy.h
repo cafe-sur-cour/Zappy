90ec113ee4583463b1b51de02b6fcbfa8169f6e5
Albane Merian <albane.merian@epitech.eu>
2025-06-13 18:08:35 +0200

    ♻️  : Server : Add a subfile for player data handling that holds all the function regarding the player's id

--- server/include/zappy.h
+++ server/include/zappy.h
@@ -102,12 +102,11 @@ int start_protocol(zappy_t *server);
 /* client.c */
 bool process_new_client(const char *team_name, int fd, zappy_t *server);
 team_t *add_client_to_team(const char *team_name, int fd, zappy_t *server);
-int get_next_free_id(zappy_t *server);
 void check_player_status(zappy_t *zappy);
 
 /* init_map.c */
 void init_game(zappy_t *server);
-void distribute_resources(zappy_t *z);
+int distribute_resources(zappy_t *z);
 
 /* init_team.c */
 void init_teams(zappy_t *server);
@@ -254,8 +253,11 @@ int plv(zappy_t *zappy, graph_net_t *graphic, char *message);
 int pin(zappy_t *zappy, graph_net_t *graphic, char *message);
 int sgt(zappy_t *zappy, graph_net_t *graphic, char *message);
 int sst(zappy_t *zappy, graph_net_t *graphic, char *message);
-player_t *get_player_by_id(game_t *game, int player_id);
 int send_bct_message(graph_net_t *graphic, int x, int y,
     inventory_t *inventory);
 
+/* player_id.c */
+player_t *get_player_by_id(game_t *game, int player_id);
+int get_next_free_id(zappy_t *server);
+void verify_player_id(zappy_t *zappy, player_t *player);
 #endif /* !ZAPPY_H_ */
