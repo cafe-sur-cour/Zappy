11e48e5537fe64b2ced1a50461b4bbc0302b1db8
Nolan Papa <nolan.papa@epitech.eu>
2025-06-13 17:17:39 +0200

    ✨ Containers: Add getWidth and getHeight methods to retrieve dimensions

--- gui/src/Graphic/HUD/Containers/Containers.cpp
+++ gui/src/Graphic/HUD/Containers/Containers.cpp
@@ -367,3 +367,13 @@ std::shared_ptr<Slider> Containers::addSliderPercent(
 
     return nullptr;
 }
+
+float Containers::getWidth() const
+{
+    return AContainers::getWidth();
+}
+
+float Containers::getHeight() const
+{
+    return AContainers::getHeight();
+}
