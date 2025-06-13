7cb7fa4cc0a4b3a7995180d9a00696816c96b7c5
Nolan Papa <nolan.papa@epitech.eu>
2025-06-13 17:17:55 +0200

    ✨ Observer: Add GameEventType enum and update onGameEvent method

--- gui/src/Observer/IObserver.hpp
+++ gui/src/Observer/IObserver.hpp
@@ -8,10 +8,22 @@
 #ifndef IOBSERVER_HPP_
 #define IOBSERVER_HPP_
 
+#include <string>
+
+enum class GameEventType {
+    STATE_CHANGED,
+    TEAM_WIN,
+    TEAM_DEFEAT
+};
+
 class IObserver {
     public:
         virtual ~IObserver() = default;
         virtual void update() = 0;
+        virtual void onGameEvent(GameEventType eventType, const std::string& teamName) {
+            (void)eventType;
+            (void)teamName;
+        }
 };
 
 #endif /* !IOBSERVER_HPP_ */
