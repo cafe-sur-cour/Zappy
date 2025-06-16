cba40236f2a9e4e4f0d0d1d6171359e98b075bcf
albanrss <alban.roussee@epitech.eu>
2025-06-16 12:17:41 +0200

    :recycle: AI Player: Const variable moved to the Utils file and fix lvl 1 upgrade stone quantities

--- ai/src/Player/Player.py
+++ ai/src/Player/Player.py
@@ -15,89 +15,12 @@ from src.Exceptions.Exceptions import (
     SocketException
 )
 from src.Communication.Communication import Communication
-from src.Utils.Utils import SUCCESS, FAILURE
+from src.Utils.Utils import (
+    SUCCESS,
+    FAILURE,
+    LVL_UPGRADES
+)
 from src.Logger.Logger import Logger
-
-
-LVL_UPGRADES = {
-    1: {
-        "players": 1,
-        "stones": {
-            "linemate": 1,
-            "deraumere": 1,
-            "sibur": 0,
-            "mendiane": 0,
-            "phiras": 0,
-            "thystame": 0
-        }
-    },
-    2: {
-        "players": 2,
-        "stones": {
-            "linemate": 1,
-            "deraumere": 1,
-            "sibur": 1,
-            "mendiane": 0,
-            "phiras": 0,
-            "thystame": 0
-        }
-    },
-    3: {
-        "players": 2,
-        "stones": {
-            "linemate": 2,
-            "deraumere": 0,
-            "sibur": 1,
-            "mendiane": 0,
-            "phiras": 2,
-            "thystame": 0
-        }
-    },
-    4: {
-        "players": 4,
-        "stones": {
-            "linemate": 1,
-            "deraumere": 1,
-            "sibur": 2,
-            "mendiane": 0,
-            "phiras": 1,
-            "thystame": 0
-        }
-    },
-    5: {
-        "players": 4,
-        "stones": {
-            "linemate": 1,
-            "deraumere": 2,
-            "sibur": 1,
-            "mendiane": 3,
-            "phiras": 0,
-            "thystame": 0
-        }
-    },
-    6: {
-        "players": 6,
-        "stones": {
-            "linemate": 1,
-            "deraumere": 2,
-            "sibur": 3,
-            "mendiane": 0,
-            "phiras": 1,
-            "thystame": 0
-        }
-    },
-    7: {
-        "players": 6,
-        "stones": {
-            "linemate": 2,
-            "deraumere": 2,
-            "sibur": 2,
-            "mendiane": 2,
-            "phiras": 2,
-            "thystame": 1
-        }
-    },
-}
 
 
 class Player:
--- ai/src/Utils/Utils.py
+++ ai/src/Utils/Utils.py
@@ -14,6 +14,86 @@ USAGE_STRING = "Usage: ./zappy_ai -p port -n name [-h machine]\n" \
 FAILURE = 84
 SUCCESS = 0
 
+LVL_UPGRADES = {
+    1: {
+        "players": 1,
+        "stones": {
+            "linemate": 1,
+            "deraumere": 0,
+            "sibur": 0,
+            "mendiane": 0,
+            "phiras": 0,
+            "thystame": 0
+        }
+    },
+    2: {
+        "players": 2,
+        "stones": {
+            "linemate": 1,
+            "deraumere": 1,
+            "sibur": 1,
+            "mendiane": 0,
+            "phiras": 0,
+            "thystame": 0
+        }
+    },
+    3: {
+        "players": 2,
+        "stones": {
+            "linemate": 2,
+            "deraumere": 0,
+            "sibur": 1,
+            "mendiane": 0,
+            "phiras": 2,
+            "thystame": 0
+        }
+    },
+    4: {
+        "players": 4,
+        "stones": {
+            "linemate": 1,
+            "deraumere": 1,
+            "sibur": 2,
+            "mendiane": 0,
+            "phiras": 1,
+            "thystame": 0
+        }
+    },
+    5: {
+        "players": 4,
+        "stones": {
+            "linemate": 1,
+            "deraumere": 2,
+            "sibur": 1,
+            "mendiane": 3,
+            "phiras": 0,
+            "thystame": 0
+        }
+    },
+    6: {
+        "players": 6,
+        "stones": {
+            "linemate": 1,
+            "deraumere": 2,
+            "sibur": 3,
+            "mendiane": 0,
+            "phiras": 1,
+            "thystame": 0
+        }
+    },
+    7: {
+        "players": 6,
+        "stones": {
+            "linemate": 2,
+            "deraumere": 2,
+            "sibur": 2,
+            "mendiane": 2,
+            "phiras": 2,
+            "thystame": 1
+        }
+    },
+}
+
 
 class Colors:
     BOLD = "\033[1m"
