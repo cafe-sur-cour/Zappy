1daf53d626945a7773fd69a1992cf687f641a0db
Nolan Papa <nolan.papa@epitech.eu>
2025-06-19 11:15:10 +0200

    ✨ Add pld handler for player level decrement functionality

---
+++ server/src/guiMessage/handlers/pld.c
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappipicaca
+** File description:
+** PLD handlers - Player Level Down
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include "../../../include/zappy.h"
+#include "../../../include/network.h"
+#include "../../../include/my.h"
+
+static int send_plv_message(graph_net_t *graphic, player_t *player)
+{
+    int size = 8 + int_str_len(player->id) + int_str_len(player->level);
+    char buffer[size];
+
+    snprintf(buffer, size, "plv #%d %d\n", player->id, player->level);
+    return write_message(graphic->fd, buffer);
+}
+
+int pld(zappy_t *zappy, graph_net_t *graphic, char *message)
+{
+    player_t *player = NULL;
+    int player_id = 0;
+
+    if (strlen(message) < 6) {
+        error_message("Invalid pld message format.");
+        return -1;
+    }
+    if (sscanf(message, "pld #%d", &player_id) != 1) {
+        error_message("Invalid pld pattern.");
+        return -1;
+    }
+    player = get_player_by_id(zappy->game, player_id);
+    if (!player)
+        return -1;
+
+    if (player->level > 1) {
+        player->level -= 1;
+        send_player_level(zappy, player);
+    }
+
+    return send_plv_message(graphic, player);
+}
