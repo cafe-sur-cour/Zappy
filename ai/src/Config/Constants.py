1a841380d9190d3c51aa584525f0b559bc94fa14
albanrss <alban.roussee@epitech.eu>
2025-06-24 09:15:21 +0200

    :building_construction: AI: Add configuration constants and level upgrade definitions

---
+++ ai/src/Config/Constants.py
@@ -0,0 +1,27 @@
+##
+## EPITECH PROJECT, 2025
+## zappy
+## File description:
+## Constants
+##
+
+USAGE_STRING = "Usage: ./zappy_ai -p port -n name [-h machine]\n" \
+               "option\t\tdescription\n" \
+               "-p port\t\tport number\n" \
+               "-n name\t\tname of the team\n" \
+               "-h machine\tname of the machine; localhost by default"
+
+FAILURE = 84
+SUCCESS = 0
+
+
+class Colors:
+    BOLD = "\033[1m"
+    RED = "\033[1m\033[31m"
+    GREEN = "\033[1m\033[32m"
+    YELLOW = "\033[1m\033[33m"
+    BLUE = "\033[1m\033[34m"
+    MAGENTA = "\033[1m\033[35m"
+    CYAN = "\033[1m\033[36m"
+    WHITE = "\033[1m\033[37m"
+    RESET = "\033[0m"
--- ai/src/Config/GameConfig.py
+++ ai/src/Config/GameConfig.py
@@ -2,17 +2,8 @@
 ## EPITECH PROJECT, 2025
 ## zappy
 ## File description:
-## Utils
+## GameConfig
 ##
-
-USAGE_STRING = "Usage: ./zappy_ai -p port -n name [-h machine]\n" \
-               "option\t\tdescription\n" \
-               "-p port\t\tport number\n" \
-               "-n name\t\tname of the team\n" \
-               "-h machine\tname of the machine; localhost by default"
-
-FAILURE = 84
-SUCCESS = 0
 
 LVL_UPGRADES = {
     1: {
@@ -102,15 +93,3 @@ TOTAL_NEEDED_STONES = {
     "phiras": 6,
     "thystame": 1
 }
-
-
-class Colors:
-    BOLD = "\033[1m"
-    RED = "\033[1m\033[31m"
-    GREEN = "\033[1m\033[32m"
-    YELLOW = "\033[1m\033[33m"
-    BLUE = "\033[1m\033[34m"
-    MAGENTA = "\033[1m\033[35m"
-    CYAN = "\033[1m\033[36m"
-    WHITE = "\033[1m\033[37m"
-    RESET = "\033[0m"
