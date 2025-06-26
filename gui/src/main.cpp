1c3b088b51a0eec8aab8744e43b6f7a691b8a887
eliotttesnier <eliott.tesnier@epitech.eu>
2025-06-26 14:55:27 +0200

    :sparkles: GUI: ctrl+c catcher

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -7,13 +7,36 @@
 
 #include <iostream>
 #include <string>
+#include <signal.h>
+#include <memory>
 
 #include "Utils/Constants.hpp"
 #include "Client/Client.hpp"
 #include "Exceptions/Exceptions.hpp"
 
+std::unique_ptr<Client> g_client = nullptr;
+
+void signalHandler(int signal)
+{
+    if (signal == SIGINT) {
+        std::cout << "\n" << colors::T_YELLOW << "Received Ctrl+C, shutting down gracefully..."
+                  << colors::RESET << std::endl;
+
+        if (g_client) {
+            g_client->stop();
+            g_client.reset();
+        }
+
+        std::cout << colors::T_GREEN << "Cleanup completed, exiting..."
+                  << colors::RESET << std::endl;
+        exit(zappy::constants::SUCCESS_EXIT_CODE);
+    }
+}
+
 int main(int ac, char **av)
 {
+    signal(SIGINT, signalHandler);
+
     if (ac == 2 && std::string(av[1]) == "-help") {
         std::cout << colors::T_CYAN << zappy::constants::USAGE_STRING
                   << colors::RESET << std::endl;
@@ -21,7 +44,7 @@ int main(int ac, char **av)
     }
 
     try {
-        Client client(ac, av);
+        g_client = std::make_unique<Client>(ac, av);
     } catch (const Exceptions::CLIParsingException &e) {
         std::cerr << colors::T_RED << e.what()
                   << colors::RESET << std::endl;
@@ -33,5 +56,7 @@ int main(int ac, char **av)
                   << e.what() << colors::RESET << std::endl;
         return zappy::constants::FAILURE_EXIT_CODE;
     }
+
+    g_client.reset();
     return zappy::constants::SUCCESS_EXIT_CODE;
 }
